0cc62faf51d13ec6172ae92e85924d0a1d12316d
David Catalan <40354404+catalan-adobe@users.noreply.github.com>
2023-06-01 15:18:56 +0200

    feat: add web functions to download and parse robots.txt and sitemaps (#21)

---
+++ src/web/sitemap.ts
@@ -0,0 +1,76 @@
+/* eslint import/no-unresolved: "off" */
+/* TODO - eslint is complaining even though the import works at runtime */
+import got from 'got';
+import { XMLParser } from 'fast-xml-parser';
+import path from 'path';
+import zlib from 'zlib';
+
+type Sitemap = {
+  url: string;
+  sitemaps?: Sitemap[];
+  lastMod?: string;
+  urls?: string[];
+};
+
+// parse a text string into an XML DOM object
+function parseXMLSitemap(sitemapContent) {
+  const options = {
+    ignoreAttributes: false,
+  };
+
+  const parser = new XMLParser(options);
+  const jsonObj = parser.parse(sitemapContent);
+
+  return jsonObj;
+}
+
+export async function parseSitemapFromUrl(
+  url: string,
+  options: { timeout?: number } = {},
+): Promise<Sitemap> {
+  try {
+    let sitemapRaw;
+
+    const reqOptions = {
+      timeout: {
+        request: options.timeout || 10000,
+      },
+      responseType: 'text',
+    };
+
+    if (path.extname(url) === '.gz') {
+      // unzip if needed
+      let response;
+
+      try {
+        reqOptions.responseType = 'buffer';
+        // @ts-expect-error - got options type is not correct
+        response = await got(url, reqOptions);
+
+        sitemapRaw = zlib.gunzipSync(response.body).toString();
+      } catch (e) {
+        sitemapRaw = response.body;
+      }
+    } else {
+      // @ts-expect-error - got options type is not correct
+      const response = await got(url, reqOptions);
+      sitemapRaw = response.body;
+    }
+
+    const sitemapObject = parseXMLSitemap(sitemapRaw);
+
+    const sitemaps = sitemapObject.sitemapindex?.sitemap?.map((element) => ({
+      url: element.loc,
+      lastMod: element.lastmod,
+    }));
+
+    const urls = sitemapObject.urlset?.url ? sitemapObject.urlset?.url?.map((element) => ({
+      url: element.loc,
+      lastMod: element.lastmod,
+    })) : [];
+
+    return { url, sitemaps, urls };
+  } catch (e) {
+    throw new Error(`parseSitemapFromUrl: ${e}`);
+  }
+}
