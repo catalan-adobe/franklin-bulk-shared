43669e8112c03982d4244466674270df76fa4102
David Catalan <catalan@adobe.com>
2023-12-22 13:16:19 +0100

    feat: use existing project configuration for lighthouse execution

--- src/puppeteer/steps/step-run-lighthouse.ts
+++ src/puppeteer/steps/step-run-lighthouse.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /*
  * Copyright 2022 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
@@ -9,7 +10,11 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import lighthouse from 'lighthouse';
+import lighthouse, { Flags } from 'lighthouse';
+// @ts-nocheck
+import desktopConfig from 'lighthouse/core/config/lr-desktop-config.js';
+// @ts-nocheck
+import mobileConfig from 'lighthouse/core/config/lr-mobile-config.js';
 
 /* eslint-disable-next-line import/prefer-default-export */
 export function runLighthouseCheck() {
@@ -18,8 +23,28 @@ export function runLighthouseCheck() {
       // main action
       await action(params);
 
+      const lhOptions: Flags = {
+        logLevel: 'error',
+        output: 'json',
+        disableFullPageScreenshot: true,
+        detailed: true,
+        chromeFlags: [
+          '--disable-gpu', // disable gpu acceleration
+          '--headless', // run chrome in headless mode
+          '--no-sandbox', // disable chrome sandbox mode
+          '--no-zygote', // disable the zygote process (https://chromium.googlesource.com/chromium/src/+/HEAD/docs/linux/zygote.md)
+          '--no-first-run', // disable first run beacon (e.g. first run wizard)
+          '--disable-storage-reset', //  disable resetting the local storage at the end
+          '--disable-features=TranslateUI,BlinkGenPropertyTrees', // disable some unnecessary chrome features
+          '--max-wait-for-load=120000', // LH waits up to 20s for the page to load
+        ],
+        onlyCategories: ['accessibility', 'best-practices', 'performance', 'seo'],
+      };
+
+      const lhConfig = params.type === 'desktop' ? desktopConfig : mobileConfig;
+
       // run lighthouse
-      const { lhr } = await lighthouse(params.url, undefined, undefined, params.page);
+      const resp = await lighthouse(params.url, lhOptions, lhConfig, params.page);
 
       params.lighthouse = {
         version: lhr.lighthouseVersion,
