a6cabd90b619424e350b5611f5775f7df106941a
David Catalan <catalan@adobe.com>
2023-12-22 14:08:16 +0100

    chore: remove obsolete browser step function

--- src/puppeteer/steps/steps.ts
+++ src/puppeteer/steps/steps.ts
@@ -2,7 +2,6 @@ export * from './step-post-load-wait.js';
 export * from './step-smart-scroll.js';
 export * from './step-collect-webconsole.js';
 export * from './step-fullpage-screenshot.js';
-export * from './step-gdpr-autoconsent.js';
 export * from './step-exec-async.js';
 export * from './step-run-lighthouse.js';
 export * from './step-cache-resources.js';
