301121c3ac88826b5295a2f6767d458befb65288
david catalan <catalan@adobe.com>
2024-03-19 17:13:35 +0100

    feat: implement test stack with coverage

---
+++ test/url.spec.js
@@ -0,0 +1,39 @@
+/* global describe, it */
+
+import { isValid, isValidHTTP } from '../src/url.js';
+import { expect } from 'chai';
+
+describe('isValid', () => {
+  it('should return a URL object for valid URLs', () => {
+    expect(isValid('http://example.com')).to.be.a('URL');
+    expect(isValid('https://example.com')).to.be.a('URL');
+  });
+
+  it('should return null for invalid URLs', () => {
+    expect(isValid('01.02.24')).to.equal(null);
+    expect(isValid('example.com')).to.equal(null);
+  });
+
+  it('should return a URL object for URLs with supported protocols', () => {
+    expect(isValid('http://example.com', ['http:'])).to.be.a('URL');
+    expect(isValid('https://example.com', ['https:'])).to.be.a('URL');
+  });
+
+  it('should return null for URLs with unsupported protocols', () => {
+    expect(isValid('http://example.com', ['https:'])).to.equal(null);
+    expect(isValid('https://example.com', ['http:'])).to.equal(null);
+  });
+});
+
+describe('isValidHTTP', () => {
+  it('should return a URL object for valid HTTP URLs', () => {
+    expect(isValidHTTP('http://example.com')).to.be.a('URL');
+    expect(isValidHTTP('https://example.com')).to.be.a('URL');
+  });
+
+  it('should return null for invalid HTTP URLs', () => {
+    expect(isValidHTTP('ftp://example.com')).to.equal(null);
+    expect(isValidHTTP('file://example.com')).to.equal(null);
+    expect(isValidHTTP('example.com')).to.equal(null);
+  });
+});
