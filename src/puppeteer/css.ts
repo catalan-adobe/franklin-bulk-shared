e3988fd4e6b32c641662d158373ad1fdd3c07d8a
david catalan <catalan@adobe.com>
2024-11-21 14:21:03 +0100

    feat: improve font detection and extraction process, support more cases

--- src/puppeteer/css.ts
+++ src/puppeteer/css.ts
@@ -14,6 +14,8 @@ import fs from 'fs';
 import fontpie from 'fontpie-calc';
 import { Browser, Page } from 'puppeteer-core';
 import path from 'path';
+import { smartScroll } from './puppeteer.js';
+import { Time } from '../../index.js';
 
 const DEFAULT_TAGS_TO_APPEND = [
   'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'button', 'img', 'p', 'span', 'b', 'strong',
@@ -245,209 +247,319 @@ export async function getMinimalCSSForCurrentPage(page: Page) {
   };
 }
 
-export async function getMinimalCSSForAEMBoilerplateFromCurrentPage(page: Page, fontsFolder = './fonts') {
-  if (!fs.existsSync(fontsFolder)) {
-    fs.mkdirSync(fontsFolder);
-  }
+export async function getMinimalCSSForAEMBoilerplateFromURL(url: string, page: Page, { logger, fontsFolder = './fonts' }) {
+  try {
+    // throw new Error('Not implemented');
+    logger.info('setting up browser page for desktop resolution ...');
+    // add listener for font updates
+    await page.setRequestInterception(true);
+    const client = await page.createCDPSession();
+    await client.send('DOM.enable');
+    await client.send('CSS.enable');
 
-  // add listener for font updates
-  const client = await page.createCDPSession();
-  await client.send('DOM.enable');
-  await client.send('CSS.enable');
-
-  // collect fonts
-  const fonts = [];
-  client.on('CSS.fontsUpdated', (e) => {
-    if (e.font) {
-      fonts.push(e);
-    }
-  });
-
-  await page.setViewport({ width: 1440, height: 1080 });
-  await page.reload();
-
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  const computedStylesForDefaultElements: any = await getCSSValuesForDefaultElements(page);
-
-  const bodyFontFamilySet = computedStylesForDefaultElements.p[0]['font-family'];
-  const headingFontFamilySet = computedStylesForDefaultElements.h1[0]['font-family'];
-
-  let fontFaces = '';
-  let fontFBFaces = '';
-  const fontFacesArr = [];
-  const fontFBFacesArr = [];
-
-  fonts.forEach(async (f) => {
-    const { font } = f;
-
-    if (font.src.includes('data:application/x-font-woff;base64')) {
-      console.log('Embedded font detected:', font.src.slice(0, 100));
-      const fontData = font.src.split('data:application/x-font-woff;base64,').pop();
-      const fontDataBuffer = Buffer.from(fontData, 'base64');
-      // sanitize font family name
-      const fontFilename = `${font.fontFamily.replace(/[^a-z0-9]/gi, '-')}-${font.fontWeight}-${font.fontStyle}.woff`.toLowerCase();
-      fs.writeFileSync(`${fontsFolder}/${fontFilename}`, fontDataBuffer);
-
-      const fb = computeFallbackFont(`${fontsFolder}/${fontFilename}`, font.fontFamily, font.fontWeight, font.fontStyle);
-      fontFBFacesArr.push(fb);
-      fontFBFaces += `@font-face {
-  font-family: '${fb.fontFamily} Fallback';
-  font-style: ${fb.fontStyle};
-  font-weight: ${fb.fontWeight};
-  src: local('${fb.fallbackFont}');
-  ascent-override: ${fb.ascentOverride};
-  descent-override: ${fb.descentOverride};
-  line-gap-override: ${fb.lineGapOverride};
-  size-adjust: ${fb.sizeAdjust};
-}
-`;
-      fontFaces += `@font-face {
-  font-family: '${font.fontFamily}';      
-  src: url('../fonts/${fontFilename}') format('woff');
-  font-weight: ${font.fontWeight};
-  font-style: ${font.fontStyle};
-  font-display: ${font.fontDisplay};
-  unicode-range: ${font.unicodeRange};
-}
-
-`;
-      fontFacesArr.push({
-        ...font,
-        src: `url('../fonts/${fontFilename}') format('woff')`,
-        location: `${fontsFolder}/${fontFilename}`,
-      });
-    } else if (font.src.startsWith('http')) {
-      console.log('External font detected:', font.src);
-      const resp = await fetch(font.src);
-      const fontDataBuffer = await resp.arrayBuffer();
-
-      // sanitize font family name
-      const u = new URL(font.src);
-      const p = path.parse(u.pathname);
-      const ext = p.ext?.replace('.', '') || 'woff';
-      const fontFilename = `${font.fontFamily.replace(/[^a-z0-9]/gi, '-')}-${font.fontWeight}-${font.fontStyle}.${ext}`.toLowerCase();
-      if (!fs.existsSync(`${fontsFolder}/${fontFilename}`)) {
-        fs.writeFileSync(`${fontsFolder}/${fontFilename}`, new Uint8Array(fontDataBuffer));
+    // collect fonts
+    const fonts = [];
+    client.on('CSS.fontsUpdated', (e) => {
+      if (e.font) {
+        if (!fonts.find((f) => f.font.fontFamily === e.font.fontFamily
+          && f.font.src === e.font.src
+          && f.font.fontWeight === e.font.fontWeight
+          && f.font.fontStyle === e.font.fontStyle
+          && f.font.unicodeRange === e.font.unicodeRange)
+        ) {
+          logger.silly('new font detected:');
+          Object.keys(e.font).forEach((key) => {
+            logger.silly(`* ${key}: ${e.font[key].length > 150 ? `${e.font[key].slice(0, 50)}...${e.font[key].slice(e.font[key].length - 50, e.font[key].length)}` : e.font[key]}`);
+          });
+          fonts.push(e);
+        }
       }
+    });
 
-      const fb = computeFallbackFont(`${fontsFolder}/${fontFilename}`, font.fontFamily, font.fontWeight, font.fontStyle);
-      fontFBFacesArr.push(fb);
-      fontFBFaces += `@font-face {
-  font-family: '${fb.fontFamily} Fallback';
-  font-style: ${fb.fontStyle};
-  font-weight: ${fb.fontWeight};
-  src: local('${fb.fallbackFont}');
-  ascent-override: ${fb.ascentOverride};
-  descent-override: ${fb.descentOverride};
-  line-gap-override: ${fb.lineGapOverride};
-  size-adjust: ${fb.sizeAdjust};
-}
-`;
+    // delay font requests
+    const fontsRequests = [];
+    page.on('request', async (interceptedRequest) => {
+      if (interceptedRequest.isInterceptResolutionHandled()) return;
+      if (interceptedRequest.resourceType() === 'font') {
+        if (!fontsRequests.find((f) => f.url === interceptedRequest.url())) {
+          fontsRequests.push({
+            url: interceptedRequest.url(),
+            done: false,
+          });
+          await Time.sleep(5000);
+          if (interceptedRequest.isInterceptResolutionHandled()) return;
+        }
+      }
+      interceptedRequest.continue();
+    });
 
-      fontFaces += `@font-face {
-  font-family: '${font.fontFamily}';      
-  src: url('../fonts/${fontFilename}') format('${ext}');');
-  font-weight: ${font.fontWeight};
-  font-style: ${font.fontStyle};
-  font-display: ${font.fontDisplay};
-  unicode-range: ${font.unicodeRange};
-}
+    page.on('requestfailed', (request) => {
+      if (request.resourceType() === 'font') {
+        const idx = fontsRequests.findIndex((f) => f.url === request.url());
+        if (idx >= 0) {
+          fontsRequests[idx].done = true;
+        }
+      }
+    });
 
-`;
-      fontFacesArr.push({
-        ...font,
-        src: `url('../fonts/${fontFilename}') format('${ext}')`,
-        location: `${fontsFolder}/${fontFilename}`,
-      });
+    page.on('requestfinished', (request) => {
+      if (request.resourceType() === 'font') {
+        const idx = fontsRequests.findIndex((f) => f.url === request.url());
+        if (idx >= 0) {
+          fontsRequests[idx].done = true;
+        }
+      }
+    });
+
+    page.on('requestservedfromcache', (request) => {
+      if (request.resourceType() === 'font') {
+        const idx = fontsRequests.findIndex((f) => f.url === request.url());
+        if (idx >= 0) {
+          fontsRequests[idx].done = true;
+        }
+      }
+    });
+
+    // setup page state
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    await Time.sleep(2000);
+    await smartScroll(page, { postReset: true });
+    await Time.sleep(1000);
+
+    // wait for all requests to be done in fontsRequests
+    let allDone = false;
+    while (!allDone) {
+      allDone = !fontsRequests.find((f) => !f.done);
+      /* eslint-disable-next-line no-await-in-loop */
+      await Time.sleep(500);
     }
-  });
 
-  // compute css values for mobile view
-  await page.setViewport({ width: 600, height: 1080 });
-  await page.reload();
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  const computedStylesForDefaultElementsMobile: any = await getCSSValuesForDefaultElements(page);
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    const computedStylesForDefaultElements: any = await getCSSValuesForDefaultElements(page);
 
-  console.log(`
-/*
-  Main fonts sets:
-  Adapt variables in styles/styles.css:
-*/
+    // compute font family usage for body font ('p')
+    const bodyFFSets = computedStylesForDefaultElements.p.reduce((acc, val) => {
+      if (acc[val['font-family']]) {
+        acc[val['font-family']] += 1;
+      } else {
+        acc[val['font-family']] = 1;
+      }
+      return acc;
+    }, {});
+    // get entry with max occurences
+    const bodyMax = Object.keys(bodyFFSets).reduce(
+      (a, b) => (bodyFFSets[a] > bodyFFSets[b] ? a : b),
+    );
+    logger.silly(`computed body font-family: ${bodyMax}`);
 
---body-font-family: ${bodyFontFamilySet};
---heading-font-family: ${headingFontFamilySet};
+    // compute font family usage for heading font ('h1', 'h2', 'h3')
+    const headingFFSets = {};
+    ['h1', 'h2', 'h3'].forEach((tag) => {
+      computedStylesForDefaultElements[tag].reduce((acc, val) => {
+        if (acc[val['font-family']]) {
+          acc[val['font-family']] += 1;
+        } else {
+          acc[val['font-family']] = 1;
+        }
+        return acc;
+      }, headingFFSets);
+    });
+    // get entry with max occurences
+    const headingMax = Object.keys(headingFFSets).reduce(
+      (a, b) => (headingFFSets[a] > headingFFSets[b] ? a : b),
+    );
+    logger.silly(`computed heading font-family: ${headingMax}`);
 
-/*
-  Fonts Definitions
-  Adapt styles/fonts.css:
-  (Do not to forget to add the generated font files (under ./fonts folder) into the Github repository)
-*/
+    await Time.sleep(1000);
 
-${fontFaces}
+    const bodyFontFamilySet = bodyMax;
+    const headingFontFamilySet = headingMax;
 
-/*
-  Fonts Fallbacks
-  Adapt styles/fonts.css:
-  (Do not to forget to add the fallback font to the --body-font-family and --heading-font-family variables)
-*/
+    let fontFaces = '';
+    let fontFBFaces = '';
+    const fontFacesArr = [];
+    const fontFBFacesArr = [];
 
-${fontFBFaces}
+    fonts.forEach(async (f) => {
+      const { font } = f;
 
-/*
-  Heading Sizes
-  Adapt variables in styles/styles.css:
-*/
+      if (font.src.includes('data:application/x-font-woff;base64')) {
+        // console.log('Embedded font detected:', font.src.slice(0, 100));
+        const fontData = font.src.split('data:application/x-font-woff;base64,').pop();
+        const fontDataBuffer = Buffer.from(fontData, 'base64');
+        // sanitize font family name
+        const fontFilename = `${font.fontFamily.replace(/[^a-z0-9]/gi, '-')}-${font.fontWeight}-${font.fontStyle}.woff`.toLowerCase();
+        fs.writeFileSync(`${fontsFolder}/${fontFilename}`, fontDataBuffer);
 
-/* desktop */
---heading-font-size-xxl: ${computedStylesForDefaultElements.h1[0]['font-size']};
---heading-font-size-xl: ${computedStylesForDefaultElements.h3[0]['font-size']};
---heading-font-size-l: ${computedStylesForDefaultElements.h3[0]['font-size']};
---heading-font-size-m: ${computedStylesForDefaultElements.h4[0]['font-size']};
---heading-font-size-s: ${computedStylesForDefaultElements.h5[0]['font-size']};
---heading-font-size-xs: ${computedStylesForDefaultElements.h6[0]['font-size']};
+        const fb = computeFallbackFont(`${fontsFolder}/${fontFilename}`, font.fontFamily, font.fontWeight, font.fontStyle);
+        fontFBFacesArr.push(fb);
+        fontFBFaces += `@font-face {
+    font-family: '${fb.fontFamily} Fallback';
+    font-style: ${fb.fontStyle};
+    font-weight: ${fb.fontWeight};
+    src: local('${fb.fallbackFont}');
+    ascent-override: ${fb.ascentOverride};
+    descent-override: ${fb.descentOverride};
+    line-gap-override: ${fb.lineGapOverride};
+    size-adjust: ${fb.sizeAdjust};
+  }
+  `;
+        fontFaces += `@font-face {
+    font-family: '${font.fontFamily}';      
+    src: url('../fonts/${fontFilename}') format('woff');
+    font-weight: ${font.fontWeight};
+    font-style: ${font.fontStyle};
+    font-display: ${font.fontDisplay};
+    unicode-range: ${font.unicodeRange};
+  }
+  
+  `;
+        fontFacesArr.push({
+          ...font,
+          src: `url('../fonts/${fontFilename}') format('woff')`,
+          location: `${fontsFolder}/${fontFilename}`,
+        });
+      } else if (font.src.startsWith('http')) {
+        const resp = await fetch(font.src);
+        const fontDataBuffer = await resp.arrayBuffer();
 
-/* mobile */
---heading-font-size-xxl: ${computedStylesForDefaultElementsMobile.h1[0]['font-size']};
---heading-font-size-xl: ${computedStylesForDefaultElementsMobile.h2[0]['font-size']};
---heading-font-size-l: ${computedStylesForDefaultElementsMobile.h3[0]['font-size']};
---heading-font-size-m: ${computedStylesForDefaultElementsMobile.h4[0]['font-size']};
---heading-font-size-s: ${computedStylesForDefaultElementsMobile.h5[0]['font-size']};
---heading-font-size-xs: ${computedStylesForDefaultElementsMobile.h6[0]['font-size']};
+        // sanitize font family name
+        const u = new URL(font.src);
+        const p = path.parse(u.pathname);
+        const ext = p.ext?.replace('.', '') || 'woff';
+        const fontFilename = `${font.fontFamily.replace(/[^a-z0-9]/gi, '-')}-${font.fontWeight}-${font.fontStyle}.${ext}`.toLowerCase();
+        if (!fs.existsSync(`${fontsFolder}/${fontFilename}`)) {
+          fs.writeFileSync(`${fontsFolder}/${fontFilename}`, new Uint8Array(fontDataBuffer));
+        }
 
-`);
+        const fb = computeFallbackFont(`${fontsFolder}/${fontFilename}`, font.fontFamily, font.fontWeight, font.fontStyle);
+        fontFBFacesArr.push(fb);
+        fontFBFaces += `@font-face {
+    font-family: '${fb.fontFamily} Fallback';
+    font-style: ${fb.fontStyle};
+    font-weight: ${fb.fontWeight};
+    src: local('${fb.fallbackFont}');
+    ascent-override: ${fb.ascentOverride};
+    descent-override: ${fb.descentOverride};
+    line-gap-override: ${fb.lineGapOverride};
+    size-adjust: ${fb.sizeAdjust};
+  }
+  `;
 
-  return {
-    bodyFontFamilySet,
-    headingFontFamilySet,
-    fontFaces: fontFacesArr,
-    fontFBFaces: fontFBFacesArr.filter((value1, i) => fontFBFacesArr.findIndex(
-      (value2) => JSON.stringify(value2) === JSON.stringify(value1),
-    ) === i),
-    headingFontSizes: {
-      xs: {
-        desktop: computedStylesForDefaultElements.h6[0]['font-size'],
-        mobile: computedStylesForDefaultElementsMobile.h6[0]['font-size'],
+        fontFaces += `@font-face {
+    font-family: '${font.fontFamily}';      
+    src: url('../fonts/${fontFilename}') format('${ext}');');
+    font-weight: ${font.fontWeight};
+    font-style: ${font.fontStyle};
+    font-display: ${font.fontDisplay};
+    unicode-range: ${font.unicodeRange};
+  }
+  
+  `;
+        fontFacesArr.push({
+          ...font,
+          src: `url('../fonts/${fontFilename}') format('${ext}')`,
+          location: `${fontsFolder}/${fontFilename}`,
+        });
+      }
+    });
+
+    logger.info('setting up browser page for mobile resolution ...');
+
+    // compute css values for mobile view
+    const mobilePage = await page.browser().newPage();
+    await mobilePage.setViewport({ width: 600, height: 1080 });
+    await mobilePage.goto(url, { waitUntil: 'networkidle2' });
+
+    logger.info('computing CSS values...');
+
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    const computedStylesForDefaultElementsMobile: any = await getCSSValuesForDefaultElements(page);
+
+    logger.silly(`
+  /*
+    Main fonts sets:
+    Adapt variables in styles/styles.css:
+  */
+  
+  --body-font-family: ${bodyFontFamilySet};
+  --heading-font-family: ${headingFontFamilySet};
+  
+  /*
+    Fonts Definitions
+    Adapt styles/fonts.css:
+    (Do not to forget to add the generated font files (under ./fonts folder) into the Github repository)
+  */
+  
+  ${fontFaces}
+  
+  /*
+    Fonts Fallbacks
+    Adapt styles/fonts.css:
+    (Do not to forget to add the fallback font to the --body-font-family and --heading-font-family variables)
+  */
+  
+  ${fontFBFaces}
+  
+  /*
+    Heading Sizes
+    Adapt variables in styles/styles.css:
+  */
+  
+  /* desktop */
+  --heading-font-size-xxl: ${computedStylesForDefaultElements.h1[0]['font-size']};
+  --heading-font-size-xl: ${computedStylesForDefaultElements.h3[0]['font-size']};
+  --heading-font-size-l: ${computedStylesForDefaultElements.h3[0]['font-size']};
+  --heading-font-size-m: ${computedStylesForDefaultElements.h4[0]['font-size']};
+  --heading-font-size-s: ${computedStylesForDefaultElements.h5[0]['font-size']};
+  --heading-font-size-xs: ${computedStylesForDefaultElements.h6[0]['font-size']};
+  
+  /* mobile */
+  --heading-font-size-xxl: ${computedStylesForDefaultElementsMobile.h1[0]['font-size']};
+  --heading-font-size-xl: ${computedStylesForDefaultElementsMobile.h2[0]['font-size']};
+  --heading-font-size-l: ${computedStylesForDefaultElementsMobile.h3[0]['font-size']};
+  --heading-font-size-m: ${computedStylesForDefaultElementsMobile.h4[0]['font-size']};
+  --heading-font-size-s: ${computedStylesForDefaultElementsMobile.h5[0]['font-size']};
+  --heading-font-size-xs: ${computedStylesForDefaultElementsMobile.h6[0]['font-size']};
+  
+  `);
+
+    return {
+      bodyFontFamilySet,
+      headingFontFamilySet,
+      fontFaces: fontFacesArr,
+      fontFBFaces: fontFBFacesArr.filter((value1, i) => fontFBFacesArr.findIndex(
+        (value2) => JSON.stringify(value2) === JSON.stringify(value1),
+      ) === i),
+      headingFontSizes: {
+        xs: {
+          desktop: computedStylesForDefaultElements.h6[0]['font-size'],
+          mobile: computedStylesForDefaultElementsMobile.h6[0]['font-size'],
+        },
+        s: {
+          desktop: computedStylesForDefaultElements.h5[0]['font-size'],
+          mobile: computedStylesForDefaultElementsMobile.h5[0]['font-size'],
+        },
+        m: {
+          desktop: computedStylesForDefaultElements.h4[0]['font-size'],
+          mobile: computedStylesForDefaultElementsMobile.h4[0]['font-size'],
+        },
+        l: {
+          desktop: computedStylesForDefaultElements.h3[0]['font-size'],
+          mobile: computedStylesForDefaultElementsMobile.h3[0]['font-size'],
+        },
+        xl: {
+          desktop: computedStylesForDefaultElements.h2[0]['font-size'],
+          mobile: computedStylesForDefaultElementsMobile.h2[0]['font-size'],
+        },
+        xxl: {
+          desktop: computedStylesForDefaultElements.h1[0]['font-size'],
+          mobile: computedStylesForDefaultElementsMobile.h1[0]['font-size'],
+        },
       },
-      s: {
-        desktop: computedStylesForDefaultElements.h5[0]['font-size'],
-        mobile: computedStylesForDefaultElementsMobile.h5[0]['font-size'],
-      },
-      m: {
-        desktop: computedStylesForDefaultElements.h4[0]['font-size'],
-        mobile: computedStylesForDefaultElementsMobile.h4[0]['font-size'],
-      },
-      l: {
-        desktop: computedStylesForDefaultElements.h3[0]['font-size'],
-        mobile: computedStylesForDefaultElementsMobile.h3[0]['font-size'],
-      },
-      xl: {
-        desktop: computedStylesForDefaultElements.h2[0]['font-size'],
-        mobile: computedStylesForDefaultElementsMobile.h2[0]['font-size'],
-      },
-      xxl: {
-        desktop: computedStylesForDefaultElements.h1[0]['font-size'],
-        mobile: computedStylesForDefaultElementsMobile.h1[0]['font-size'],
-      },
-    },
-  };
+    };
+  } catch (error) {
+    logger.error(error);
+    return null;
+  }
 }
